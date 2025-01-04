6eed25aebbbd846fb17fcd7605c26921656b46a1
worldOfVerdure <perspicaciousdotard@gmail.com>
2025-01-03 23:47:05 -0600

    Minor css fixes

--- JS/script.js
+++ JS/script.js
@@ -258,31 +258,31 @@ function resolveDieRoll(player) {
 
     switch (RAND_NUM) {
       case 1:
-        dieImgP1.src = "images/oneDieP1.svg";
+        dieImgP1.src = "../images/oneDieP1.svg";
         dieImgP1.alt = "Player 1 die with value of 1";
         break;
       case 2:
-        dieImgP1.src = "images/twoDieP1.svg";
+        dieImgP1.src = "../images/twoDieP1.svg";
         dieImgP1.alt = "Player 1 die with value of 2";
         break;
       case 3:
-        dieImgP1.src = "images/threeDieP1.svg";
+        dieImgP1.src = "../images/threeDieP1.svg";
         dieImgP1.alt = "Player 1 die with value of 3";
         break;
       case 4:
-        dieImgP1.src = "images/fourDieP1.svg";
+        dieImgP1.src = "../images/fourDieP1.svg";
         dieImgP1.alt = "Player 1 die with value of 4";
         break;
       case 5:
-        dieImgP1.src = "images/fiveDieP1.svg";
+        dieImgP1.src = "../images/fiveDieP1.svg";
         dieImgP1.alt = "Player 1 die with value of 5";
         break;
       case 6:
-        dieImgP1.src = "images/sixDieP1.svg";
+        dieImgP1.src = "../images/sixDieP1.svg";
         dieImgP1.alt = "Player 1 die with value of 6";
         break;
       default:
-        dieImgP1.src = "images/emptyDie.svg";
+        dieImgP1.src = "../images/emptyDie.svg";
         dieImgP1.alt = "A blank die for player 1"; 
     }
   }
@@ -291,31 +291,31 @@ function resolveDieRoll(player) {
 
     switch (RAND_NUM) {
       case 1:
-        dieImgP2.src = "images/oneDieP2.svg";
+        dieImgP2.src = "../images/oneDieP2.svg";
         dieImgP2.alt = "Player 2 die with value of 1";
         break;
       case 2:
-        dieImgP2.src = "images/twoDieP2.svg";
+        dieImgP2.src = "../images/twoDieP2.svg";
         dieImgP2.alt = "Player 2 die with value of 2";
         break;
       case 3:
-        dieImgP2.src = "images/threeDieP2.svg";
+        dieImgP2.src = "../images/threeDieP2.svg";
         dieImgP2.alt = "Player 2 die with value of 3";
         break;
       case 4:
-        dieImgP2.src = "images/fourDieP2.svg";
+        dieImgP2.src = "../images/fourDieP2.svg";
         dieImgP2.alt = "Player 2 die with value of 4";
         break;
       case 5:
-        dieImgP2.src = "images/fiveDieP2.svg";
+        dieImgP2.src = "../images/fiveDieP2.svg";
         dieImgP2.alt = "Player 2 die with value of 5";
         break;
       case 6:
-        dieImgP2.src = "images/sixDieP2.svg";
+        dieImgP2.src = "../images/sixDieP2.svg";
         dieImgP2.alt = "Player 2 die with value of 6";
         break;
       default:
-        dieImgP2.src = "images/emptyDie.svg";
+        dieImgP2.src = "../images/emptyDie.svg";
         dieImgP2.alt = "A blank die for player 2"; 
     }
   }
@@ -326,6 +326,7 @@ function startRound(flag) {
   matchConcludeMenu.style.display = "none";
   // Remove color for player in winner menu.
   winner.removeAttribute("class");
+  resetDice();
   resetScores();
   player1.setHoldStatus = player1.getHoldStatus;
   player2.setHoldStatus = player2.getHoldStatus;
@@ -352,3 +353,16 @@ function resetScores () {
   player1Score.innerText = player1.getScore;
   player2Score.innerText = player2.getScore;
 }
+
+function resetDice() {
+  dieImgP1.src = "../images/emptyDie.svg";
+  dieImgP1.alt = "A blank die for player 1";
+  dieImgP2.src = "../images/emptyDie.svg";
+  dieImgP2.alt = "A blank die for player 2"; 
+}
+
+/*
+!Why don't the buttons grey out on mobile?
+!Why is Hold not centered in its button?
+!Why is my flag not showing up?
+*/
